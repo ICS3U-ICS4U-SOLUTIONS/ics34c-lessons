9c7cdf3c5768da1907d82287112d93be7bb4ea7a
e28166 <e28166@FXH-0015840124>
2025-05-20 11:42:01 -0400

    s

---
+++ lessons/Math.py
@@ -0,0 +1,25 @@
+# Math Operations
+
+# Addition
+num1 = 3
+num2 = 4
+answer = num1 + num2
+print(answer)
+
+# Subtraction
+print(5-2)
+
+# Multiplication
+print(3*3)
+
+# Division (always returns float)
+print(4/2)
+
+# Integer division returns integer, does not round answer)
+print(5//2)
+
+# Modulus (gives remainder)
+print(5%2)
+
+# Exponentiation
+print(2**3)
---
+++ lessons/Variables.py
@@ -0,0 +1,22 @@
+# variables
+myInteger = 0
+myDouble = 0.0
+myString = ""
+myBoolean1 = False
+myBoolean2 = True
+
+# get integer from user then print it
+print("Enter an integer: ")
+myInteger = input()
+print("You entered the number: " + myInteger)
+
+# get double from user then print it
+print("Enter a double: ")
+myDouble = input()
+print("You entered the number: " + myDouble)
+
+# get String from user then print it
+print("Enter a String: ")
+myString = input()
+print("You entered the String: " + myString)
+
