46fd4b5e012a3bddae167eb7e490ab2862290fab
e28166 <e28166@FXH-0015840124>
2025-05-22 12:21:46 -0400

    s

---
+++ lessons/Decisions.py
@@ -0,0 +1,15 @@
+# Conditions
+# >, <, <=, ==, >=, !=
+
+mark = -5
+
+if mark >= 50:
+    print("You passed.")
+
+elif mark < 50 and mark >= 0:
+    print("You failed")
+    
+else:
+    print("Invalid grade.")
+    
+    
---
+++ lessons/MathModule.py
@@ -0,0 +1,17 @@
+import math
+import random
+
+# returns greatest common factor of a and b
+print(math.gcd(8, 4))
+
+# returns the ceiling of x
+print(math.ceil(3.0))
+
+# returns the floor of x
+print(math.floor(4.9))
+
+# random number between x and y
+print(random.randint( 1, 6))
+
+# random decimal less than 1
+print(random.random())
