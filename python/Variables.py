8b5c729fe2ef1fbc8c233d7570529c39294c7530
e28166 <e28166@10.20.1.185>
2025-09-11 10:18:32 -0400

    s

---
+++ python/Variables.py
@@ -0,0 +1,10 @@
+# variables
+age = 16
+height = 169
+shoeSize = 9
+carModel = "Beetle"
+myAddress = "124 Main Street"
+
+# NOTES
+# Variables should always be in camelCase format.
+# This means each word is capitalized, except the first word
