078317750e8291662ba9e2d426c30c76acb58e2c
e28166 <e28166@DESKTOP-56C86U1>
2025-10-22 09:43:09 -0400

    f

---
+++ src/lessons/ComparisonOperators.java
@@ -0,0 +1,22 @@
+package lessons;
+
+public class ComparisonOperators {
+
+	public static void main(String[] args) {
+
+		// comparison operators:
+		// >, >=, <, <=, ==, != 
+		
+		// variables
+		int num1 = 13;
+		int num2 = 7;
+		
+		if (num1 >= num2)  {
+			
+			System.out.println("Greater than or equal to.");
+		}
+		
+
+	}
+
+}
