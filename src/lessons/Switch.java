f68f6df963aff2b30553522b163122bfd81e9435
e28166 <e28166@DESKTOP-56C86U1>
2025-10-22 09:54:43 -0400

    s

---
+++ src/lessons/Switch.java
@@ -0,0 +1,31 @@
+package lessons;
+
+public class Switch {
+
+	public static void main(String[] args) {
+
+		int choice = 1;
+		
+		switch (choice)  {
+		
+		case 1:
+			System.out.println("Case 1 matched.");
+			break;
+			
+		case 2:
+			System.out.println("Case 2 matched.");
+			break;
+			
+		case 3:
+			System.out.println("Case 3 matched.");
+			break;
+		
+		default:
+			System.out.println("Default matched.");
+		
+		}
+		
+
+	}
+
+}
