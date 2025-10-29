21aaeeb64adc9a8e9620eaad559520aae0454f69
e28166 <e28166@DESKTOP-56C86U1>
2025-10-29 10:15:54 -0400

    s

---
+++ src/lessons/VoidMethods.java
@@ -0,0 +1,17 @@
+package lessons;
+
+public class VoidMethods {
+
+	public static void main(String[] args) {
+	
+		// this calls the method below
+		myFirstMethod();
+	}
+	
+	
+	// void methods do not return anything
+	public static void myFirstMethod()  {
+	
+		System.out.println("Hello World!");
+	}
+}
