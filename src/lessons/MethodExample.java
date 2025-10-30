a71e30b01b722f11f4d0d910ca0241696e89354f
e28166 <e28166@DESKTOP-56C86U1>
2025-10-30 10:55:38 -0400

    s

---
+++ src/lessons/MethodExample.java
@@ -0,0 +1,23 @@
+package lessons;
+
+public class MethodExample {
+
+	public static void main(String[] args) {
+		
+		// variables
+		int answer = 0;
+		
+		// print answer
+		System.out.println(Math.max(1, 2));
+		
+		// keep answer
+		int answer = Math.max(1, 2);
+		
+		System.out.println(answer);
+
+	}
+
+	
+	
+	
+}
